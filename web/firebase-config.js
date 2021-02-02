05c56b6225169615589a8c03204911d4c40edd08
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2021-02-02 09:18:22 +0530

    added firebase config files

---
+++ web/firebase-config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDCPJfmW4Nn0U3qVgT_KatItS0I9nIJZIs",
+    authDomain: "astronomy-log-book.firebaseapp.com",
+    projectId: "astronomy-log-book",
+    storageBucket: "astronomy-log-book.appspot.com",
+    messagingSenderId: "703059723550",
+    appId: "1:703059723550:web:a271bb6eb147bdef22750f",
+    measurementId: "G-31HXDMKFHT"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
