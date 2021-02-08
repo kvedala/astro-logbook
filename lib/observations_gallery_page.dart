c38aa2c1d9c7e2142f3f0cfbcf00d626f25f12fe
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2021-02-08 09:50:15 +0530

    added mssier, ngc and date search queries

--- lib/observations_gallery_page.dart
+++ lib/observations_gallery_page.dart
@@ -3,48 +3,145 @@ import 'package:astro_log/gallary_tile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 /// Page to display the observations as a gallery
-class ObservationsGallary extends StatelessWidget {
+class ObservationsGallary extends StatefulWidget {
+  _ObservationsGallaryState createState() => _ObservationsGallaryState();
+}
+
+class _ObservationsGallaryState extends State<ObservationsGallary> {
+  final ngcSearchController = TextEditingController();
+  final messierSearchController = TextEditingController();
+  final stringSearchController = TextEditingController();
+  final dateSearchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    return StreamBuilder<QuerySnapshot>(
-      stream: _getObservations(),
-      builder: (context, snap) => snap.connectionState ==
-              ConnectionState.waiting
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : Column(children: [
-              Container(
-                  padding: EdgeInsets.symmetric(vertical: 10),
-                  child:
-                      Text("Number of Observations: ${snap.data.docs.length}")),
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: size.width > 845
-                        ? 6
-                        : size.width >= 670
-                            ? 4
-                            : size.width >= 550
-                                ? 3
-                                : 2,
+    return Column(children: [
+      Container(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: messierSearchController,
+                keyboardType: TextInputType.numberWithOptions(
+                    decimal: false, signed: false),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search_rounded, color: Colors.red),
+                  labelText: "Search Messier",
+                  suffix: SizedBox(
+                    height: 15,
+                    width: 15,
+                    child: IconButton(
+                      padding: EdgeInsets.all(0),
+                      icon: Icon(Icons.clear_rounded, size: 15),
+                      onPressed: () =>
+                          setState(() => messierSearchController.clear()),
+                    ),
                   ),
-                  shrinkWrap: true,
-                  itemCount: snap.data.docs.length,
-                  itemBuilder: (context, index) => snap.data == null
-                      ? SizedBox()
-                      : GallaryTile.fromObservation(
-                          ObservationData.fromJSON(
-                              snap.data.docs[index].data()),
-                        ),
                 ),
+                onChanged: (value) => setState(() {}),
               ),
-            ]),
-    );
+            ),
+            Expanded(
+              child: TextField(
+                controller: ngcSearchController,
+                keyboardType: TextInputType.numberWithOptions(
+                    decimal: false, signed: false),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search_rounded, color: Colors.red),
+                  labelText: "Search NGC",
+                  suffix: SizedBox(
+                    height: 15,
+                    width: 15,
+                    child: IconButton(
+                      padding: EdgeInsets.all(0),
+                      icon: Icon(Icons.clear_rounded, size: 15),
+                      onPressed: () =>
+                          setState(() => ngcSearchController.clear()),
+                    ),
+                  ),
+                ),
+                onChanged: (value) => setState(() {}),
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                controller: dateSearchController,
+                keyboardType: TextInputType.datetime,
+                decoration: InputDecoration(
+                    icon: Icon(Icons.search_rounded, color: Colors.red),
+                    labelText: "Search by Date"),
+                onChanged: (value) => setState(() {}),
+                onTap: () async {
+                  DateTime initialDate;
+                  try {
+                    initialDate =
+                        DateFormat.yMMMd().parse(dateSearchController.text);
+                  } catch (e) {
+                    initialDate = DateTime.now();
+                  }
+                  final selectedDate = await showDatePicker(
+                      context: context,
+                      initialDate: initialDate,
+                      firstDate: DateTime(1900),
+                      lastDate: DateTime.now(),
+                      fieldLabelText: "Search by date");
+                  if (selectedDate != null)
+                    setState(() => dateSearchController.text =
+                        DateFormat.yMMMd().format(selectedDate));
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      StreamBuilder<QuerySnapshot>(
+          stream: _getObservations(),
+          builder: (context, snap) {
+            switch (snap.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              default:
+                return !snap.hasData
+                    ? SizedBox()
+                    : Expanded(
+                        child: GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: size.width > 845
+                                ? 6
+                                : size.width >= 670
+                                    ? 4
+                                    : size.width >= 550
+                                        ? 3
+                                        : 2,
+                          ),
+                          shrinkWrap: true,
+                          itemCount: snap.data.docs.length,
+                          itemBuilder: (context, index) =>
+                              GallaryTile.fromObservation(
+                            ObservationData.fromJSON(
+                                snap.data.docs[index].data()),
+                          ),
+                        ),
+                      );
+            }
+          }),
+    ]);
   }
 
   /// Get user observations as a stream instead of bulk load
@@ -53,10 +150,48 @@ class ObservationsGallary extends StatelessWidget {
     final firestore = FirebaseFirestore.instance;
     final auth = FirebaseAuth.instance;
 
-    return firestore
-        .collection('users/' + auth.currentUser.uid + '/observations')
-        .orderBy('dateTime', descending: true)
-        .snapshots();
+    if (dateSearchController.text.isNotEmpty) {
+      DateTime startDate;
+      try {
+        startDate = DateFormat.yMMMd().parse(dateSearchController.text);
+      } catch (e) {
+        startDate = null;
+      }
+      if (startDate == null)
+        return firestore
+            .collection('users/' + auth.currentUser.uid + '/observations')
+            .where('messier',
+                isEqualTo: int.tryParse(messierSearchController.text))
+            .where('ngc', isEqualTo: int.tryParse(ngcSearchController.text))
+            // .where('title', arrayContains: stringSearchController.text)
+            // .where('notes', arrayContains: stringSearchController)
+            // .orderBy('dateTime', descending: true)
+            .snapshots(includeMetadataChanges: true);
+      startDate = DateTime(startDate.year, startDate.month, startDate.day);
+      final endDate =
+          DateTime(startDate.year, startDate.month, startDate.day + 1);
+      return firestore
+          .collection('users/' + auth.currentUser.uid + '/observations')
+          .where('dateTime', isGreaterThanOrEqualTo: startDate)
+          .where('dateTime', isLessThanOrEqualTo: endDate)
+          // .orderBy('dateTime', descending: true)
+          .snapshots(includeMetadataChanges: true);
+    } else if (messierSearchController.text.isNotEmpty ||
+        ngcSearchController.text.isNotEmpty)
+      return firestore
+          .collection('users/' + auth.currentUser.uid + '/observations')
+          .where('messier',
+              isEqualTo: int.tryParse(messierSearchController.text))
+          .where('ngc', isEqualTo: int.tryParse(ngcSearchController.text))
+          // .where('title', arrayContains: stringSearchController.text)
+          // .where('notes', arrayContains: stringSearchController)
+          // .orderBy('dateTime', descending: true)
+          .snapshots(includeMetadataChanges: true);
+    else
+      return firestore
+          .collection('users/' + auth.currentUser.uid + '/observations')
+          .orderBy('dateTime', descending: true)
+          .snapshots(includeMetadataChanges: true);
     // .map<List<ObservationData>>(
     //     (snapshot) => snapshot.docs.map<ObservationData>((doc) {
     //           return ObservationData.fromJSON(doc.data());
