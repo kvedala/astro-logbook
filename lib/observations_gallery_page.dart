ff9254ad11f165b059f6470c7799731c8d50fd88
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2021-02-16 12:02:13 +0530

    added ability to select and filter results

--- lib/observations_gallery_page.dart
+++ lib/observations_gallery_page.dart
@@ -1,11 +1,10 @@
-import 'package:astro_log/add_observation.dart';
-import 'package:astro_log/gallary_tile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'utils.dart';
+import 'add_observation.dart';
+import 'filter_widget.dart';
+import 'gallary_tile.dart';
 
 /// Page to display the observations as a gallery
 class ObservationsGallary extends StatefulWidget {
@@ -15,13 +14,6 @@ class ObservationsGallary extends StatefulWidget {
 }
 
 class _ObservationsGallaryState extends State<ObservationsGallary> {
-  final ngcSearchController = TextEditingController();
-  final messierSearchController = TextEditingController();
-  final stringSearchController = TextEditingController();
-  final dateSearchController = TextEditingController();
-  DateTimeRange dateSearchRange;
-  bool clickOnClear = false;
-
   // @override
   // void initState() {
   //   super.initState();
@@ -32,152 +24,90 @@ class _ObservationsGallaryState extends State<ObservationsGallary> {
     final size = MediaQuery.of(context).size;
 
     return Column(children: [
-      Container(
-        padding: EdgeInsets.symmetric(vertical: 0),
-        height: 60,
-        child: Row(
-          children: [
-            Container(
-              width: 100,
-              child: TextField(
-                controller: messierSearchController,
-                keyboardType: TextInputType.numberWithOptions(
-                    decimal: false, signed: false),
-                decoration: InputDecoration(
-                  // prefixIcon: Icon(Icons.search_rounded, color: Colors.red),
-                  labelText: "Messier#",
-                  isDense: true,
-                  suffix: IconButton(
-                    padding: EdgeInsets.all(0),
-                    iconSize: 20,
-                    icon: Icon(Icons.clear_rounded),
-                    onPressed: () {
-                      setState(() => messierSearchController.clear());
-                      FocusScope.of(context).unfocus();
-                    },
-                  ),
-                ),
-                onChanged: (value) {
-                  setState(() {});
-                  Future.delayed(
-                      Duration(seconds: 2), FocusScope.of(context).unfocus);
-                },
-              ),
-            ),
-            Container(
-              width: 100,
-              child: TextField(
-                controller: ngcSearchController,
-                keyboardType: TextInputType.numberWithOptions(
-                    decimal: false, signed: false),
-                decoration: InputDecoration(
-                  // prefixIcon: Icon(Icons.search_rounded, color: Colors.red),
-                  labelText: "NGC#",
-                  isDense: true,
-                  suffix: IconButton(
-                    padding: EdgeInsets.all(0),
-                    iconSize: 20,
-                    icon: Icon(Icons.clear_rounded),
-                    onPressed: () {
-                      setState(() => ngcSearchController.clear());
-                      FocusScope.of(context).unfocus();
-                    },
-                  ),
-                ),
-                onChanged: (value) {
-                  setState(() {});
-                  Future.delayed(
-                      Duration(seconds: 2), FocusScope.of(context).unfocus);
-                },
-              ),
-            ),
-            Expanded(
-              child: TextField(
-                controller: dateSearchController,
-                keyboardType: TextInputType.datetime,
-                readOnly: true,
-                decoration: InputDecoration(
-                  // prefixIcon: Icon(Icons.search_rounded, color: Colors.red),
-                  labelText: "Date Range",
-                  isDense: true,
-                  suffix: IconButton(
-                    padding: EdgeInsets.all(0),
-                    iconSize: 20,
-                    icon: Icon(Icons.clear_rounded),
-                    onPressed: () => setState(() {
-                      clickOnClear = true;
-                      dateSearchController.clear();
-                      dateSearchRange = null;
-                      FocusScope.of(context).unfocus();
-                    }),
-                  ),
-                ),
-                onChanged: (value) => setState(() {}),
-                onTap: () async {
-                  if (clickOnClear) {
-                    clickOnClear = false;
-                    return;
-                  }
-                  dateSearchRange = await showDateRangePicker(
-                    context: context,
-                    initialDateRange: dateSearchRange ?? null,
-                    firstDate: DateTime(1950),
-                    lastDate: DateTime.now(),
-                    fieldStartLabelText: "From date",
-                    fieldEndLabelText: "End date",
-                  );
-                  if (dateSearchRange != null)
-                    setState(() => dateSearchController.text =
-                        dateSearchRange.start.yMMMd +
-                            " - " +
-                            dateSearchRange.end.yMMMd);
-                  else
-                    setState(() => dateSearchController.clear());
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-      StreamBuilder<QuerySnapshot>(
-          stream: _getObservations(),
-          builder: (context, snap) {
-            switch (snap.connectionState) {
-              case ConnectionState.none:
-                // case ConnectionState.waiting:
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              default:
-                return !snap.hasData
-                    ? SizedBox()
-                    : Expanded(
-                        child: GridView.builder(
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: size.width > 845
-                                ? 6
-                                : size.width >= 670
-                                    ? 4
-                                    : size.width >= 550
-                                        ? 3
-                                        : 2,
+      ObservationTabBar(() => setState(() {})),
+      searchState['onlySearch']
+          ? FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('users/' +
+                      FirebaseAuth.instance.currentUser.uid +
+                      '/observations')
+                  .get()
+                  .then((snap) => snap.docs
+                      .where((doc) => selectedTiles.contains(doc.reference))
+                      .toList(growable: false)),
+              builder: (context, snap) => snap.connectionState !=
+                      ConnectionState.done
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : !snap.hasData
+                      ? SizedBox()
+                      : Expanded(
+                          child: GridView.builder(
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: size.width > 845
+                                  ? 6
+                                  : size.width >= 670
+                                      ? 4
+                                      : size.width >= 550
+                                          ? 3
+                                          : 2,
+                            ),
+                            shrinkWrap: true,
+                            itemCount: snap.data.length,
+                            itemBuilder: (context, index) =>
+                                index >= snap.data.length
+                                    ? null
+                                    : GallaryTile.fromObservation(
+                                        ObservationData.fromJSON(
+                                          snap.data[index].data(),
+                                        ),
+                                        reference: snap.data[index].reference,
+                                      ),
                           ),
-                          shrinkWrap: true,
-                          itemCount: snap.data.docs.length,
-                          itemBuilder: (context, index) => index <
-                                  snap.data.docs.length
-                              ? GallaryTile.fromObservation(
-                                  ObservationData.fromJSON(
-                                    snap.data.docs[index].data(),
-                                  ),
-                                  reference: snap.data.docs[index].reference,
-                                )
-                              : null,
                         ),
-                      );
-            }
-          }),
+            )
+          : StreamBuilder<QuerySnapshot>(
+              stream: _getObservations(),
+              builder: (context, snap) {
+                switch (snap.connectionState) {
+                  case ConnectionState.none:
+                    // case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  default:
+                    return !snap.hasData
+                        ? SizedBox()
+                        : Expanded(
+                            child: GridView.builder(
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: size.width > 845
+                                    ? 6
+                                    : size.width >= 670
+                                        ? 4
+                                        : size.width >= 550
+                                            ? 3
+                                            : 2,
+                              ),
+                              shrinkWrap: true,
+                              itemCount: snap.data.docs.length,
+                              itemBuilder: (context, index) =>
+                                  index >= snap.data.docs.length
+                                      ? null
+                                      : GallaryTile.fromObservation(
+                                          ObservationData.fromJSON(
+                                            snap.data.docs[index].data(),
+                                          ),
+                                          reference:
+                                              snap.data.docs[index].reference,
+                                        ),
+                            ),
+                          );
+                }
+              }),
     ]);
   }
 
@@ -187,21 +117,19 @@ class _ObservationsGallaryState extends State<ObservationsGallary> {
     final firestore = FirebaseFirestore.instance;
     final auth = FirebaseAuth.instance;
 
-    if (dateSearchRange != null)
+    if (searchState['date'] != null)
       return firestore
           .collection('users/' + auth.currentUser.uid + '/observations')
-          .where('dateTime', isGreaterThanOrEqualTo: dateSearchRange.start)
-          .where('dateTime', isLessThanOrEqualTo: dateSearchRange.end)
+          .where('dateTime', isGreaterThanOrEqualTo: searchState['date'].start)
+          .where('dateTime', isLessThanOrEqualTo: searchState['date'].end)
           // .orderBy('dateTime', descending: true)
           .snapshots(includeMetadataChanges: true);
     // .get();
-    else if (messierSearchController.text.isNotEmpty ||
-        ngcSearchController.text.isNotEmpty)
+    else if (searchState['messier'].isNotEmpty || searchState['ngc'].isNotEmpty)
       return firestore
           .collection('users/' + auth.currentUser.uid + '/observations')
-          .where('messier',
-              isEqualTo: int.tryParse(messierSearchController.text))
-          .where('ngc', isEqualTo: int.tryParse(ngcSearchController.text))
+          .where('messier', isEqualTo: int.tryParse(searchState['messier']))
+          .where('ngc', isEqualTo: int.tryParse(searchState['ngc']))
           // .where('title', arrayContains: stringSearchController.text)
           // .where('notes', arrayContains: stringSearchController)
           // .orderBy('dateTime', descending: true)
