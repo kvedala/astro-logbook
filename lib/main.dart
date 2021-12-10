52e206a1937994ee736cd2f632e1916c8c798c02
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2021-12-09 23:17:13 -0500

    upgrade firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 import 'routes.dart';
 import 'sign_in.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -13,70 +17,59 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // Create the initialization Future outside of `build`:
-    final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+    return MaterialApp(
+      title: 'Astronomy Log Book',
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      // ),
+      darkTheme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+            bodyColor: Colors.red,
+            displayColor: Colors.red,
+            decorationColor: Colors.red),
+        primaryIconTheme: IconThemeData(color: Colors.red),
+        primaryColorBrightness: Brightness.dark,
+        // accentIconTheme: IconThemeData(color: Colors.red),
+        // floatingActionButtonTheme: FloatingActionButtonThemeData(),
+        brightness: Brightness.dark,
+        // buttonColor: ButtonThemeData(textTheme: ButtonTextTheme.accent),
+        iconTheme: IconThemeData(color: Colors.red),
+        inputDecorationTheme:
+            InputDecorationTheme(labelStyle: TextStyle(color: Colors.red)),
+        unselectedWidgetColor: Colors.red,
 
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) throw "Initialization error: ${snapshot.error}";
-        if (snapshot.connectionState == ConnectionState.waiting)
-          return Center(child: CircularProgressIndicator());
-        return MaterialApp(
-          title: 'Astronomy Log Book',
-          // theme: ThemeData(
-          //   primarySwatch: Colors.blue,
-          // ),
-          darkTheme: ThemeData(
-            textTheme: Theme.of(context).textTheme.apply(
-                bodyColor: Colors.red,
-                displayColor: Colors.red,
-                decorationColor: Colors.red),
-            primaryIconTheme: IconThemeData(color: Colors.red),
-            primaryColorBrightness: Brightness.dark,
-            // accentIconTheme: IconThemeData(color: Colors.red),
-            // floatingActionButtonTheme: FloatingActionButtonThemeData(),
-            brightness: Brightness.dark,
-            // buttonColor: ButtonThemeData(textTheme: ButtonTextTheme.accent),
-            iconTheme: IconThemeData(color: Colors.red),
-            inputDecorationTheme:
-                InputDecorationTheme(labelStyle: TextStyle(color: Colors.red)),
-            unselectedWidgetColor: Colors.red,
-
-            colorScheme: ColorScheme.dark(
-              primary: Colors.red.shade800,
-              secondary: Colors.red.shade600,
-              primaryVariant: Colors.red.shade600,
-              secondaryVariant: Colors.red.shade600,
-            ),
-            appBarTheme: AppBarTheme(
-              toolbarTextStyle: Theme.of(context)
-                  .textTheme
-                  .apply(
-                      bodyColor: Colors.red,
-                      displayColor: Colors.red,
-                      decorationColor: Colors.red)
-                  .bodyText2,
-              titleTextStyle: Theme.of(context)
-                  .textTheme
-                  .apply(
-                      bodyColor: Colors.red,
-                      displayColor: Colors.red,
-                      decorationColor: Colors.red)
-                  .headline6,
-            ),
-            tabBarTheme: TabBarTheme(
-              labelColor: Colors.red,
-              indicator: UnderlineTabIndicator(
-                  borderSide: BorderSide(color: Colors.red)),
-            ),
-          ),
-          themeMode: ThemeMode.dark,
-          // home: MyHomePage(title: 'Astronomy Log Book'),
-          initialRoute: SignInPageRoute,
-          routes: routeMap,
-        );
-      },
+        colorScheme: ColorScheme.dark(
+          primary: Colors.red.shade800,
+          secondary: Colors.red.shade600,
+          primaryVariant: Colors.red.shade600,
+          secondaryVariant: Colors.red.shade600,
+        ),
+        appBarTheme: AppBarTheme(
+          toolbarTextStyle: Theme.of(context)
+              .textTheme
+              .apply(
+                  bodyColor: Colors.red,
+                  displayColor: Colors.red,
+                  decorationColor: Colors.red)
+              .bodyText2,
+          titleTextStyle: Theme.of(context)
+              .textTheme
+              .apply(
+                  bodyColor: Colors.red,
+                  displayColor: Colors.red,
+                  decorationColor: Colors.red)
+              .headline6,
+        ),
+        tabBarTheme: TabBarTheme(
+          labelColor: Colors.red,
+          indicator:
+              UnderlineTabIndicator(borderSide: BorderSide(color: Colors.red)),
+        ),
+      ),
+      themeMode: ThemeMode.dark,
+      // home: MyHomePage(title: 'Astronomy Log Book'),
+      initialRoute: SignInPageRoute,
+      routes: routeMap,
     );
   }
 }
