8637901dabdf7f5a2107450e3b7720149e5fe8c5
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2022-01-15 16:02:25 -0500

    cleanup and add rise & set times to objects

--- lib/objects.dart
+++ lib/objects.dart
@@ -28,7 +28,7 @@ abstract class Catalog extends StatelessWidget {
   /// was this object viewed by the user
   final bool viewed;
 
-  late final RiseSetTimes? _riseTimes;
+  late final RiseSetTimes? riseTimes;
 
   Catalog(this.id, this.ra, this.dec,
       {required this.name,
@@ -38,22 +38,22 @@ abstract class Catalog extends StatelessWidget {
       this.viewed = false,
       gps.LocationData? currentLocation}) {
     if (currentLocation == null)
-      _riseTimes = null;
+      riseTimes = null;
     else {
-      _riseTimes = RiseSetTimes.forObject(ra, dec, currentLocation);
+      riseTimes = RiseSetTimes.forObject(ra, dec, currentLocation);
     }
   }
 
   @override
   Widget build(BuildContext context) {
     late Widget visible;
-    if (_riseTimes == null)
+    if (riseTimes == null)
       visible = SizedBox();
-    else if (_riseTimes!.belowHorizon)
+    else if (riseTimes!.belowHorizon)
       visible = Icon(Icons.cancel);
-    else if (_riseTimes!.circumpolar ||
-        _riseTimes!.riseTime!.hour >= 18 ||
-        _riseTimes!.setTime!.hour <= 5) {
+    else if (riseTimes!.circumpolar ||
+        riseTimes!.riseTime!.hour >= 18 ||
+        riseTimes!.setTime!.hour <= 5) {
       visible = Icon(Icons.done);
     } else {
       visible = Icon(Icons.cancel);
@@ -70,24 +70,24 @@ abstract class Catalog extends StatelessWidget {
         TableRow(children: [
           Text("RA: ${ra.toString()}"),
           Text(type),
-          _riseTimes == null
+          riseTimes == null
               ? SizedBox()
-              : _riseTimes!.circumpolar
+              : riseTimes!.circumpolar
                   ? Text("Circumpolar")
-                  : _riseTimes!.belowHorizon
+                  : riseTimes!.belowHorizon
                       ? Text("Below Horizon")
                       : Text(
-                          "Rise: ${DateFormat("HH:mm").format(_riseTimes!.riseTime!)}"),
+                          "Rise: ${DateFormat("HH:mm").format(riseTimes!.riseTime!)}"),
         ]),
         TableRow(children: [
           Text("DEC: ${dec.toString()}"),
           Text(difficulty ?? ""),
-          _riseTimes == null
+          riseTimes == null
               ? SizedBox()
-              : (_riseTimes!.circumpolar | _riseTimes!.belowHorizon)
+              : (riseTimes!.circumpolar | riseTimes!.belowHorizon)
                   ? SizedBox()
                   : Text(
-                      "Set: ${DateFormat("HH:mm").format(_riseTimes!.setTime!)}"),
+                      "Set: ${DateFormat("HH:mm").format(riseTimes!.setTime!)}"),
         ]),
       ]),
       trailing: visible,
@@ -210,121 +210,4 @@ class Caldwell extends Catalog {
         "dec": dec.json,
         "difficulty": difficulty.toString()
       };
-}
-
-/// http://www2.arnes.si/~gljsentvid10/sidereal.htm
-///
-/// Shamelessly translated from
-/// https://github.com/codebox/star-rise-and-set-times/blob/master/calc.js
-// class _RiseSetTimes {
-//   final DateTime? riseTime;
-//   final DateTime? setTime;
-//   final bool belowHorizon;
-//   final bool circumpolar;
-
-//   _RiseSetTimes({
-//     this.riseTime,
-//     this.setTime,
-//     this.belowHorizon = false,
-//     this.circumpolar = false,
-//   });
-
-//   // factory _RiseSetTimes.fromMillisecondsSinceEpoch({
-//   //   required num riseTimeMilliseconds,
-//   //   required num setTimeMilliseconds,
-//   //   bool belowHorizon = false,
-//   //   bool circumpolar = false,
-//   // }) =>
-//   //     _RiseSetTimes(
-//   //         riseTime:
-//   //             DateTime.fromMillisecondsSinceEpoch(riseTimeMilliseconds.round()),
-//   //         setTime:
-//   //             DateTime.fromMillisecondsSinceEpoch(setTimeMilliseconds.round()),
-//   //         belowHorizon: belowHorizon,
-//   //         circumpolar: circumpolar);
-
-//   static const MINUTES_PER_HOUR = 60;
-//   static const SECONDS_PER_HOUR = 60;
-//   static const SECONDS_PER_MINUTE = 60;
-//   static const HOURS_PER_DAY = 24;
-//   static const MILLISECONDS_PER_SECOND = 1000;
-//   static const MINUTES_PER_DAY = MINUTES_PER_HOUR * HOURS_PER_DAY;
-//   static const SECONDS_PER_DAY = SECONDS_PER_MINUTE * MINUTES_PER_DAY;
-//   static const MILLISECONDS_PER_DAY = SECONDS_PER_DAY * MILLISECONDS_PER_SECOND;
-//   static const EPOCH_MILLIS_AT_2000_01_01_12_00_00 = 946728000000;
-
-//   /// Convert [timeInHours] to a DateTime object
-//   static TimeOfDay hoursToClockTime(num timeInHours) {
-//     final hours = timeInHours.floor();
-//     final minutes = ((timeInHours - hours) * MINUTES_PER_HOUR).floor();
-//     // final seconds = ((timeInHours - hours - minutes / MINUTES_PER_HOUR) * SECONDS_PER_HOUR).floor();
-
-//     return TimeOfDay(hour: hours, minute: minutes);
-//   }
-
-//   /// find all `n` such that
-//   /// `x_0 <= (n * m + r - a) / b <= x_1`
-//   static List<num> unmod(num r, num a, num b, num m, num x_0, num x_1) {
-//     // find all 'n' such that x_0 <= (n * m + r - a) / b <= x_1
-//     final fromN = ((x_0 * b + a - r) / m).ceil();
-//     final toN = ((x_1 * b + a - r) / m).floor();
-//     final xValues = <num>[];
-//     for (var n = fromN; n <= toN; n++) {
-//       xValues.add((n * m + r - a) / b);
-//     }
-//     return xValues;
-//   }
-
-//   static TimeOfDay radiansToUtcTime(
-//       double radians, gps.LocationData userLocation) {
-//     final daysSince_2000_01_01_12 = (DateTime.now().millisecondsSinceEpoch -
-//             EPOCH_MILLIS_AT_2000_01_01_12_00_00) /
-//         MILLISECONDS_PER_DAY;
-//     final prevMidDay = (daysSince_2000_01_01_12).floor();
-//     // https://aa.usno.navy.mil/faq/docs/GAST.php
-//     final days = unmod(
-//         radians,
-//         4.894961212735792 + (userLocation.longitudeRad!),
-//         6.30038809898489,
-//         2 * pi,
-//         prevMidDay,
-//         prevMidDay + 1)[0];
-//     final millisSinceEpoch =
-//         days * MILLISECONDS_PER_DAY + EPOCH_MILLIS_AT_2000_01_01_12_00_00;
-//     final millisSinceStartOfDay = millisSinceEpoch % MILLISECONDS_PER_DAY;
-//     final hoursSinceStartOfDay =
-//         millisSinceStartOfDay / (MILLISECONDS_PER_SECOND * SECONDS_PER_HOUR);
-//     return hoursToClockTime(hoursSinceStartOfDay);
-//   }
-
-//   factory _RiseSetTimes.forObject(
-//       RightAscession ra, Declination dec, gps.LocationData location) {
-//     if ((dec.degree - location.latitude!).abs() >= 90)
-//       return _RiseSetTimes(belowHorizon: true);
-//     if ((dec.degree + location.latitude!).abs() >= 90)
-//       return _RiseSetTimes(circumpolar: true);
-
-//     final c = acos(-tan(dec.radian) * tan(location.latitudeRad!));
-//     final riseTimeRadians = ra.radian - c;
-//     final setTimeRadians = ra.radian + c;
-
-//     return _RiseSetTimes(
-//       riseTime: radiansToUtcTime(riseTimeRadians, location).toDateTimeUTC(),
-//       setTime: radiansToUtcTime(setTimeRadians, location).toDateTimeUTC(),
-//     );
-//   }
-// }
-
-extension on TimeOfDay {
-  /// Convert to [DateTime] in local timezone
-  DateTime toDateTimeUTC() {
-    final now = DateTime.now().toUtc();
-    return DateTime(
-      now.year,
-      now.month,
-      now.day,
-      this.hour,
-      this.minute,
-    ); // always in local timezone
-  }
 }
