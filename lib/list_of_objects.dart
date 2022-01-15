3063829de9e9bf69963a51d1c50904bc723f9f36
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2022-01-15 16:07:48 -0500

    clean up

--- lib/list_of_objects.dart
+++ lib/list_of_objects.dart
@@ -1,4 +1,3 @@
-import 'package:astro_log/ra_dec.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
