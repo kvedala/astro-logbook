42857874e1ed0809ed2e69835a3c0bbe4e2df991
Dr Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2022-12-08 10:06:29 -0500

    optimize weather widget

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -9,40 +9,62 @@ class WeatherPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final loader = StreamController<int>();
-    return Stack(
-      children: [
-        Center(
-          child: StreamBuilder(
-            stream: loader.stream,
-            builder: (context, snap) => !snap.hasData || snap.data == 100
-                ? const SizedBox()
-                : Column(
-                    children: [
-                      LinearProgressIndicator(value: snap.data! / 100),
-                      Text('Loading: ${snap.data}%'),
-                    ],
-                  ),
-          ),
-        ),
-        InAppWebView(
-          initialFile: 'assets/astrospheric.html',
-          onLoadStop: (controller, url) => getCurrentPosition.then(
-            (position) => controller.evaluateJavascript(
+    // final loader = StreamController<int>();
+    final screen = MediaQuery.of(context).size;
+
+    return Center(
+      child:
+          // Column(
+          //   children: [
+          //     Center(
+          //       child: StreamBuilder(
+          //         stream: loader.stream,
+          //         builder: (context, snap) => !snap.hasData || snap.data == 100
+          //             ? const SizedBox()
+          //             : Column(
+          //                 children: [
+          //                   LinearProgressIndicator(value: snap.data! / 100),
+          //                   Text('Loading: ${snap.data}%'),
+          //                 ],
+          //               ),
+          //       ),
+          //     ),
+          //     SingleChildScrollView(
+          //       child: Expanded(
+          //         child:
+          InAppWebView(
+        initialFile: 'assets/astrospheric.html',
+        onLoadStop: (controller, url) => getCurrentPosition.then(
+          (position) async {
+            await controller.callAsyncJavaScript(functionBody: """
+const d = document.getElementById('AstrosphericContainer');
+${screen.width > screen.height ? "d.style.width = '' + width + ' px'; d.style.height = '100%'" : "d.style.width = '100%'; d.style.height = '' + height + ' px"};
+console.log(d.style);"
+""", arguments: {'width': screen.width, 'height': screen.height});
+            await controller.evaluateJavascript(
                 source: 'm_AstrosphericEmbed.Create("AstrosphericContainer", '
-                    '${position.latitude}, ${position.longitude});'),
-          ),
-          initialSettings: InAppWebViewSettings(
-            // useShouldOverrideUrlLoading: true,
-            allowsInlineMediaPlayback: true,
-            sharedCookiesEnabled: true,
-            transparentBackground: true,
-            applicationNameForUserAgent:
-                'Astronomy Logbook (https://github.com/kvedala/astro-logbook)',
-          ),
-          onProgressChanged: (controller, progress) => loader.add(progress),
+                    '${position.latitude}, ${position.longitude});');
+          },
         ),
-      ],
+        onConsoleMessage: (controller, consoleMessage) =>
+            debugPrint(consoleMessage.message),
+        initialSettings: InAppWebViewSettings(
+          // useShouldOverrideUrlLoading: true,
+          allowsInlineMediaPlayback: true,
+          sharedCookiesEnabled: true,
+          transparentBackground: true,
+          verticalScrollBarEnabled: true,
+          verticalScrollbarPosition:
+              VerticalScrollbarPosition.SCROLLBAR_POSITION_RIGHT,
+
+          applicationNameForUserAgent:
+              'Astronomy Logbook (https://github.com/kvedala/astro-logbook)',
+        ),
+        // onProgressChanged: (controller, progress) => loader.add(progress),
+      ),
+      //     ),
+      //   ),
+      // ],
     );
   }
 
